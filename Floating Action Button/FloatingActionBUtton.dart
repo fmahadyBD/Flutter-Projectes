95fa7b85e5d655e8538625dd45a2eaeb0d234e83
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-24 09:24:38 +0600

    Add files via upload

---
+++ Floating Action Button/FloatingActionBUtton.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ctlr+ apace popp will show , statelew typpe
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const rootPage(),
+    );
+  }
+}
+
+//ctrl+j for terminal
+class rootPage extends StatefulWidget {
+  const rootPage({super.key});
+
+  @override
+  State<rootPage> createState() => _rootPageState();
+}
+
+// ignore: camel_case_types
+class _rootPageState extends State<rootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("The Challenge"),
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('print it');
+        },
+        child: const Icon(Icons.add),
+      ),
+
+      
+    );
+  }
+}
